c3cacbba24649949b0f2655e88fd7872fa1e6ece
master <951394653@qq.com>
2016-01-04 22:37:18 +0800

    别打我。。。工程文件刚刚创建。。我还在写一些初始的东西

---
+++ app/src/main/java/com/tesla/ai/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.tesla.ai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        new Thread(){
+
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        }.start();
+    }
+}
