95eae962976e3392ca26102a285ab1aba7b17aef
master <951394653@qq.com>
2016-01-05 20:27:38 +0800

    做好了发送窗口的界面

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
+                                           FBI warning
+
+
+`federal law provides severe civil and criminal penalties for the unauthorized reproduction,
+distribution ,or exhibition of copyrighted motion pictures (title 17,united states code,sections 501 and 508),
+the federal bureau of investigation investigates allegations of criminal copyright infringement 
+(title 17,united states code,section 506)`
+
+
 # AIAndroid is:
 受我的友人的影响 决定开始写的:<br/>
-安卓人工智能
-===
-完全没开始做。。。<br/>
+运行在[Android操作系统](http://baike.baidu.com/link?url=Fq6NnG8s-GSI9lQ3EsBQyI0Km0zICP-RA4kNTK4083ZGfQ9CBZ2Ews7V-zlmPulmX6qG7mbMYXpdpayxfPfKf7GAzQxtxLJzVYnVHG47Pw3)上的[人工智能](http://baike.baidu.com/link?url=vd1SM2trGRrWeWv5uerz1PTu1G1PTLQTBgNXTd9bDGoWizhDFFdZlR8KnHpTxZE3eLcJvV0YXs-OwDdvNWHvi6LzMWf5oRnuaj2E1BL7QOq)<br/>
 我刚刚创建Module。。。不要打我。。<br/>
 
 # 实现情况：
 
-## 算法部分
+## 算法(运行方式)部分
 - [ ] 起名字
 - [ ] 实现简单的汉字分析
 - [ ] 实现汉字语法分析
@@ -15,14 +22,14 @@
 - [ ] 在和主人的交流中学习
 - [ ] 模拟人类情绪
 
-## UI部分
-- [ ] 一个教科书式的冰封APP的标准开始界面
+## UI(视觉效果)部分
+- [X] 一个教科书式的冰封APP的标准开始界面
 - [ ] 对话由RecyclerView组成
 - [ ] 二人的发言颜色不同
 - [ ] 能发送颜文字、表情等
 - [ ] 简单的动作
 
-## 目前的规划
+## 不同类型的数据持久化的规划
 + 聊天记录保存在SQLite中
 + 知识保存在SQLite中
 + 记忆保存在Preference中
--- app/src/main/java/com/tesla/ai/LoginActivity.java
+++ app/src/main/java/com/tesla/ai/LoginActivity.java
@@ -1,13 +1,17 @@
 package com.tesla.ai;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import util.tags;
 
 public class LoginActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         toFloat();
+
+        showTitle();
 
         new Thread(){
 
@@ -39,5 +45,24 @@ public class LoginActivity extends AppCompatActivity {
         ImageView NC = (ImageView) findViewById(R.id.ncFloating);
         NC.setBackgroundResource(R.drawable.nc_floating);
         ((AnimationDrawable) NC.getBackground()).start();
+    }
+
+    private void showTitle(){
+
+        TextView title;
+        SharedPreferences preferences;
+        title = (TextView) findViewById(R.id.title);
+        // 为了向下兼容，我使出了谜之表达式
+        preferences = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                ? getSharedPreferences(tags.PREFERENCE_NAME, MODE_ENABLE_WRITE_AHEAD_LOGGING)
+                : getSharedPreferences(tags.PREFERENCE_NAME, MODE_WORLD_READABLE);
+        title.setText(preferences.getString(tags.OWNER_NAME,tags.MEET_FIRST_TIME));
+
+        if(preferences.contains(tags.OWNER_NAME)){
+            preferences.edit()
+                    .putString(tags.OWNER_NAME, tags.HAVENT_GOT_NAME)
+                    .apply();
+        }
+
     }
 }
