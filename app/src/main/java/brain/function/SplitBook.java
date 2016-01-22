ae4b545687255bf2e89da414bc18bcbb916d5c6f
千里冰封 <zhangyinsen@126.com>
2016-01-23 07:36:36 +0800

    生成一些图片.

---
+++ app/src/main/java/brain/function/SplitBook.java
@@ -0,0 +1,20 @@
+package brain.function;
+
+import android.net.Uri;
+
+/**
+ * Copyright 2016(c) Comet Corporation.
+ * Created by asus1 on 2016/1/22.
+ * 书的简化类
+ */
+public class SplitBook {
+
+    private Uri bookUri;
+
+    public SplitBook(Uri bookUri) {
+        this.bookUri = bookUri;
+    }
+
+    public SplitBook() {
+    }
+}
